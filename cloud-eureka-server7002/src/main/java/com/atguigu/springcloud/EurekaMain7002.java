8433c6fe6b65666e5a16f860edeb38f8c84ea03c
liubaojun <lbj0609@163.com>
2022-08-25 10:01:24 +0800

    EurekaServer集群搭建

---
+++ cloud-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liubaojun
+ * Date: 2022/8/25
+ * Time: 8:45
+ * Description: No Description
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
